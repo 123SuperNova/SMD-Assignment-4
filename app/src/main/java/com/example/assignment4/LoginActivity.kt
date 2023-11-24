061f57a7b1368f35589fe96f297d4d9bf94ebed5
123SuperNova <133556322+123SuperNova@users.noreply.github.com>
2023-11-24 23:58:56 +0500

    Assignment 4 Login and SignUp with Firebase

---
+++ app/src/main/java/com/example/assignment4/LoginActivity.kt
@@ -0,0 +1,274 @@
+package com.example.assignment4
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.ViewTreeObserver
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.assignment4.ui.theme.Assignment4Theme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // ...
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            Assignment4Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginPage(this, auth = auth)
+                }
+            }
+        }
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            //reload()
+            // Create an explicit intent to start SecondActivity
+            val intent = Intent(this, MainActivity::class.java)
+
+            // Add any extra data you want to pass to SecondActivity
+            intent.putExtra("key", "Hello from MainActivity!")
+
+            // Start SecondActivity
+            ContextCompat.startActivity(this, intent, null)
+        }
+    }
+}
+
+@Composable
+fun keyboardAsState(): State<Boolean> {
+    val isImeVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
+    return rememberUpdatedState(isImeVisible)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginPage(loginActivity: LoginActivity, auth: FirebaseAuth) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    val view = LocalView.current
+    val viewTreeObserver = view.viewTreeObserver
+    DisposableEffect(viewTreeObserver) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val isKeyboardOpen = ViewCompat.getRootWindowInsets(view)
+                ?.isVisible(WindowInsetsCompat.Type.ime()) ?: true
+            // ... do anything you want here with `isKeyboardOpen`
+            if (!isKeyboardOpen){
+                focusManager.clearFocus()
+            }
+        }
+
+        viewTreeObserver.addOnGlobalLayoutListener(listener)
+        onDispose {
+            viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text="Login Page",
+            fontSize=40.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+
+        // Email field
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(focusRequester)
+        )
+
+        val cPasswordVisibility = remember { mutableStateOf(true) }
+
+        // Password field
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            trailingIcon = {
+                IconButton(onClick = {
+                    cPasswordVisibility.value = !cPasswordVisibility.value
+                }) {
+                    Icon(
+                        imageVector = if (cPasswordVisibility.value) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                        contentDescription = "visibility"
+                    )
+                }
+            },
+            visualTransformation = if (cPasswordVisibility.value) PasswordVisualTransformation() else VisualTransformation.None,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(focusRequester)
+        )
+
+        // Login button
+        Button(
+            onClick = {
+                // Perform login logic here
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(loginActivity, "Please enter Email.",Toast.LENGTH_SHORT).show()
+                    return@Button;
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(loginActivity, "Please enter Password.",Toast.LENGTH_SHORT).show()
+                    return@Button;
+                }
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(loginActivity) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success")
+                            //val user = auth.currentUser
+                            // Create an explicit intent to start SecondActivity
+                            val intent = Intent(loginActivity, MainActivity::class.java)
+
+                            // Add any extra data you want to pass to SecondActivity
+                            //intent.putExtra("key", "Hello from MainActivity!")
+
+                            // Start SecondActivity
+                            ContextCompat.startActivity(loginActivity, intent, null)
+                            Toast.makeText(
+                                loginActivity,
+                                "Authentication Succeeded.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                            loginActivity.finish()
+                            //updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                loginActivity,
+                                "Authentication failed."  + task.exception?.message.toString(),
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                            return@addOnCompleteListener
+                            //updateUI(null)
+                        }
+                    }
+
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Login")
+        }
+
+        // Sign-up button
+        Button(
+            onClick = {
+                // Create an explicit intent to start SecondActivity
+                val intent = Intent(loginActivity, SignUpActivity::class.java)
+
+                // Add any extra data you want to pass to SecondActivity
+                intent.putExtra("key", "Hello from MainActivity!")
+
+                // Start SecondActivity
+                ContextCompat.startActivity(loginActivity, intent, null)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Sign Up")
+        }
+
+        // Forgot password button
+        TextButton(
+            onClick = {
+                // Navigate to the forgot password page or implement your logic
+
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Forgot Password?")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    Assignment4Theme {
+        //LoginPage()
+    }
+}
