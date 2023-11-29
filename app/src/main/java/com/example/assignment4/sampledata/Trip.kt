2f7b5cf321a282e53b42f33266cf016a36f82733
Sarb0z <93070907+Sarb0Z@users.noreply.github.com>
2023-11-29 12:07:12 -0500

    add-edit-page skeleton done

---
+++ app/src/main/java/com/example/assignment4/sampledata/Trip.kt
@@ -0,0 +1,3 @@
+package com.example.assignment4.sampledata
+import com.example.assignment4.sampledata.Activity
+data class Trip(val title: String, val description: String, val activities: List<Activity>)
